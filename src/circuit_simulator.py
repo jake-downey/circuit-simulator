9c16b7c7e354d357461a200d43af6d94ea6ee519
thunderboogie <thunderboogie4@gmail.com>
2022-03-25 17:34:11 -0600

    changed C to c

--- src/circuit_simulator.py
+++ src/circuit_simulator.py
@@ -7,34 +7,34 @@ from utils import *
 class RLC(Scene):
     r = ValueTracker(0)
     l = ValueTracker(0.2)
-    C = ValueTracker(0.0001)
+    c = ValueTracker(0.0001)
 
     EMF = 5
     I = 0
-    Q = C.get_value() * EMF
+    Q = c.get_value() * EMF
 
     dt = .001
     time = ValueTracker(1)
 
     # Returns the Voltage and Current at frame
     def get_voltage_and_current(self):
-        alpha = (self.Q / (self.l.get_value() * self.C.get_value())) - (self.I * self.r.get_value() / self.l.get_value())
+        alpha = (self.Q / (self.l.get_value() * self.c.get_value())) - (self.I * self.r.get_value() / self.l.get_value())
 
         self.I = self.I + (alpha * self.dt)
         self.Q = self.Q - (self.I * self.dt)
 
-        Vc = self.Q / self.C.get_value()
+        Vc = self.Q / self.c.get_value()
         return Vc, self.I
 
     # Generates the plot Volatage by Time
     def generate_voltage_plot(self, axes):
-        self.Q = self.C.get_value() * self.EMF
+        self.Q = self.c.get_value() * self.EMF
         self.I = 0
         return axes.plot(lambda t: self.get_voltage_and_current()[0], [0, self.time.get_value(), self.dt]).set_color(YELLOW)
 
     # Generates the plot Current by Time
     def generate_current_plot(self, axes):
-        self.Q = self.C.get_value() * self.EMF
+        self.Q = self.c.get_value() * self.EMF
         self.I = 0
         return axes.plot(lambda t: self.get_voltage_and_current()[1], [0, self.time.get_value(), self.dt]).set_color(BLUE)
 
@@ -96,7 +96,7 @@ class RLC(Scene):
         c_text, c_number, c_units = c_label = VGroup(
             Text("C = ", font_size=36).set_color(PURPLE),
             DecimalNumber(
-                self.C.get_value(),
+                self.c.get_value(),
                 num_decimal_places = 6
             ),
             Text("F", font_size=28, slant = ITALIC).set_color(WHITE)
@@ -106,7 +106,7 @@ class RLC(Scene):
         # Create Frequency Text
         frequency_text = Text("Frequency = ", font_size=36).set_color(ORANGE)
         frequency_number = DecimalNumber(
-                    get_resonant_frequency(self.l, self.C),
+                    get_resonant_frequency(self.l, self.c),
                     num_decimal_places = 2
                 )
         frequency_units = Text("Hz", font_size=28, slant = ITALIC)
@@ -122,8 +122,8 @@ class RLC(Scene):
         # add updaters to decimal numbers
         r_number.add_updater(lambda m: m.set_value(self.r.get_value()))
         l_number.add_updater(lambda m: m.set_value(self.l.get_value()))
-        c_number.add_updater(lambda m: m.set_value(self.C.get_value()))
-        frequency_number.add_updater(lambda m: m.set_value(get_resonant_frequency(self.l, self.C)))
+        c_number.add_updater(lambda m: m.set_value(self.c.get_value()))
+        frequency_number.add_updater(lambda m: m.set_value(get_resonant_frequency(self.l, self.c)))
 
         # add objects and animations
         self.add(r_label, l_label, c_label, frequency_label, va_label)
@@ -133,10 +133,10 @@ class RLC(Scene):
         self.wait()
         self.play(self.r.animate.set_value(6), run_time = 3)
         self.play(self.l.animate.set_value(0.5), run_time = 2)
-        self.play(self.C.animate.set_value(0.002), run_time = 2)
+        self.play(self.c.animate.set_value(0.002), run_time = 2)
 
         self.play(self.l.animate.set_value(0.2), run_time = 1)
-        self.play(self.C.animate.set_value(0.0001), run_time = 1)
+        self.play(self.c.animate.set_value(0.0001), run_time = 1)
         self.play(self.r.animate.set_value(0), run_time = 2)
 
         self.play(self.time.animate.set_value(2))
